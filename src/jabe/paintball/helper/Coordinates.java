893e4d6bfb7a3be7c2f7cf42886a5a45009dd540
Jan <janbernoth@gmail.com>
2014-10-15 23:12:50 +0200

    Comparable Coordinates with test

--- src/jabe/paintball/helper/Coordinates.java
+++ src/jabe/paintball/helper/Coordinates.java
@@ -1,11 +1,18 @@
 package jabe.paintball.helper;
 
-public class Coordinates {
+public class Coordinates implements Comparable<Coordinates> {
 	public int x;
 	public int y;
 	public Coordinates(int x, int y){
 		this.x = x;
 		this.y = y;
+	}
+	@Override
+	public int compareTo(Coordinates o) {
+		// compareTo should return < 0 if this is supposed to be
+        // less than other, > 0 if this is supposed to be greater than 
+        // other and 0 if they are supposed to be equal
+		return new Integer(this.x).compareTo(o.x) + new Integer(this.y).compareTo(o.y);
 	}
 	
 }
---
+++ src/jabe/paintball/test/ObstacleTest.java
@@ -0,0 +1,54 @@
+package jabe.paintball.test;
+
+import static org.junit.Assert.*;
+
+import jabe.paintball.helper.Coordinates;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+public class ObstacleTest {
+
+	@Test
+	public void testCoordinates() {
+		List<Coordinates> coords = new ArrayList<Coordinates>();
+		coords.add(new Coordinates(1, 1));
+		coords.add(new Coordinates(1, 5));
+		coords.add(new Coordinates(1, 2));
+		coords.add(new Coordinates(1, 4));
+		coords.add(new Coordinates(1, 3));
+
+		Collections.sort(coords);
+		assertEquals(1, coords.get(0).y);
+		assertEquals(5, coords.get(4).y);
+
+		coords = new ArrayList<Coordinates>();
+		coords.add(new Coordinates(1, 1));
+		coords.add(new Coordinates(5, 1));
+		coords.add(new Coordinates(2, 1));
+		coords.add(new Coordinates(4, 1));
+		coords.add(new Coordinates(3, 1));
+
+		Collections.sort(coords);
+		assertEquals(1, coords.get(0).x);
+		assertEquals(5, coords.get(4).x);
+
+		coords = new ArrayList<Coordinates>();
+		coords.add(new Coordinates(1, 1));
+		coords.add(new Coordinates(5, 7));
+		coords.add(new Coordinates(2, 0));
+		coords.add(new Coordinates(4, 3));
+		coords.add(new Coordinates(3, 6));
+
+		Collections.sort(coords);
+		assertEquals(1, coords.get(0).x);
+		assertEquals(2, coords.get(1).x);
+		assertEquals(4, coords.get(2).x);
+		assertEquals(3, coords.get(3).x);
+		assertEquals(5, coords.get(4).x);
+	}
+
+}
